77223b99fd4a1c66d2bc5761638dea9df5242345
Kumud Yadav <kumudyadav847@gmail.com>
2026-08-28 14:46:56 +0530

    Q4

--- 03_Business_analysis.sql
+++ 03_Business_analysis.sql
@@ -43,3 +43,12 @@ SELECT incident_type,
 FROM claims
 GROUP BY incident_type
 ORDER BY fraud_rate DESC;
+-- Does weather condition influence the likelihood of a fraudulent claim?
+SELECT weather_condition,
+    COUNT(*) AS total_claims,
+    SUM(is_fraud) AS fraudulent_claims,
+    round(SUM(is_fraud)::NUMERIC / COUNT(*) * 100, 2) as fraud_rate,
+    ROUND(SUM(claim_amount), 2) AS total_claim_amount
+FROM claims
+GROUP BY weather_condition
+ORDER BY fraud_rate DESC;
